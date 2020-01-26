f9809990156c6c660bacffe097d89b2704d46762
xzh <945007373@qq.com>
2020-01-26 19:28:54 +0800

    保存进度

--- push_code.sh
+++ push_code.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 
-cd ../my_gitbook/
+cd /home/ubuntu/svn_project/my_gitbook/
 # git checkout master
 # gitbook install; gitbook build
 git add .
@@ -8,13 +8,13 @@ git commit -am "保存进度"
 git push gitee_origin master
 git push origin master
 
-cp _book ~/Documents/ -r
+cp _book /home/ubuntu/Documents/ -r
 git checkout gh-pages
 if [[ $? -eq 0 ]]; then
 	rm -rf ./*
 fi
-mv ~/Documents/_book/* .
-rm -rf ~/Documents/_book
+mv /home/ubuntu/Documents//_book/* .
+rm -rf /home/ubunu/Documents/_book
 git add .
 git commit -am "保存进度"
 git push origin gh-pages
